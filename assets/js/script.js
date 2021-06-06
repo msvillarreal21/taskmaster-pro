7369a9518c1fad619c4cec42b1cfb1599d968d79
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-06-06 18:13:09 -0500

    UX enchancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,17 +71,23 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event,) {
+  activate: function(event,ui) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
-  deactivate: function(event) {
+  deactivate: function(event, ui) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
-  over: function(event) {
+  over: function(event, ) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
         // array to store the task data in
@@ -116,12 +122,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -143,7 +152,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -179,14 +188,11 @@ textInput.trigger("focus");
 });
 
   // recreate p element
-  var taskP = $("<p>").addClass("m-1").text(text);
-
-  // replace textarea with new content
+var taskP = $("<p>").addClass("m-1").text(text);
+// replace textarea with new content
   $(this).replaceWith(taskP);
 
 
-
-   
   var auditTask = function(taskEl) {
     // get date from task element
     var date = $(taskEl).find("span").text().trim();
@@ -321,3 +327,13 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setTimeout(function() {
+  alert("This messge happens after 5 seconds!");
+}, 5000);
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+},(1000 * 60) *30);
